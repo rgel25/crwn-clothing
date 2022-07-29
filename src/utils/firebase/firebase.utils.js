89cb3d23a20704c64799dcd25315fd825389209c
Argel Miralles <argelmiralles25@gmail.com>
2022-07-30 07:25:07 +0800

    fixed app not being used, blocking deployment

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,7 +21,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
